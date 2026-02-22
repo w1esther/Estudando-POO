c73d89ca57298dbcb6a0aa9de255900d469e5ad3
w1esther <w1mariaesther@gmail.com>
2026-02-22 11:41:17 -0300

    solução desafio 16

---
+++ aula6/desafio16.py
@@ -0,0 +1,22 @@
+from rich import print
+
+class Funcionario:
+
+    def __init__(self, nome, setor, cargo, empresa):
+        
+        self.nome = nome
+        self.setor = setor
+        self.cargo = cargo
+        self.empresa = empresa
+
+    def apresentacao(self):
+        
+        return f':wave: Olá sou [blue]{self.nome}[/] e sou {self.cargo} do setor {self.setor} da empresa {self.empresa}'
+    
+c1 = Funcionario('Maria', 'Administração', 'Diretora', 'Curso em Video')
+
+print(c1.apresentacao())
+
+c2 = Funcionario('Pedro', 'TI', 'Programador', 'Curso em Video')
+
+print(c2.apresentacao())
